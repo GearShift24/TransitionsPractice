dbbce1adab4e3c45484046e4b2c05ea5cab0f7f6
GearShift24 <jwri6847@740s-pl-369.local>
2016-10-03 07:56:59 -0600

    linked an added view to its associated viewcontrollr.swift

---
+++ TransitionsPractice/UpdatedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  UpdatedViewController.swift
+//  TransitionsPractice
+//
+//  Created by Wrigley, Joseph on 10/3/16.
+//  Copyright © 2016 Ctec. All rights reserved.
+//
+
+import UIKit
+
+class UpdatedViewController : UIViewController
+{
+    override func viewDidLoad()
+    
+    {
+        super.viewDidLoad()
+        //Load this pages components.
+    }
+}
+
